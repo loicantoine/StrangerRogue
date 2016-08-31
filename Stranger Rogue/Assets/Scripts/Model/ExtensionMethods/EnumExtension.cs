70a5a62fabb7e77353df5be5d9125193d3bd8611
loicantoine <antoineloic@gmail.com>
2016-08-31 21:25:08 +0200

    Add project

---
+++ Stranger Rogue/Assets/Scripts/Model/ExtensionMethods/EnumExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class EnumExtension
+{
+  public static bool HasFlag(this NeighbourOccupancy cell, NeighbourOccupancy testedFlag)
+  {
+    return (cell & testedFlag) == testedFlag;
+  }
+}
